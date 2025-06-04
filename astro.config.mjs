99e8d0a81f193186a75b65ddadf70a08b31c0899
fabri <nohaymasintentos@gmail.com>
2025-06-04 16:08:44 -0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site:'https://shevek1.github.io',
+  base: '/usyco-fix',
   vite: {
     plugins: [tailwindcss()]
   }

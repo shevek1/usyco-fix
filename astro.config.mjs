041b6e3a00d231c712f7ef0ebc21f2b7aed909b6
fabri <nohaymasintentos@gmail.com>
2025-06-04 16:18:59 -0300

    nav y faq a components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site:'https://shevek1.github.io',
   base: '/usyco-fix',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
